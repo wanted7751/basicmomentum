1e2dac2afea7a0fceb8e43cb9eee1efd971d4da9
parkminho <minho4377@daum.net>
2019-01-05 07:19:38 +0900

    #3.6 Making a To Do List part Two

--- todo.js
+++ todo.js
@@ -2,19 +2,35 @@ const toDoForm = document.querySelector(".js-toDoform"),
   toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".js-toDoList");
 
-console.log(toDoInput);
-
 const TODOS_LS = "toDos"
+
+const toDos = [
+
+];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 
 function paintTodo(text){
     const li = document.createElement("li");
     const delteBtn = document.createElement("button");
-    delteBtn.innerHTML = "❌";
     const span = document.createElement("span");
+    const newId = toDos.length + 1
+    delteBtn.innerHTML = "❌";
     span.innerText = text
     li.appendChild(span);
     li.appendChild(delteBtn);
+    li.id= newId;
     toDoList.appendChild(li)
+    const toDoobj={
+        text: text,
+        id: newId
+    }
+    toDos.push(toDoobj);
+    saveToDos();
+    // saveToDos 는 toDos.push(toDoobj) 이거 후에 호출 해야 한다. 안그러게되면 비어있게 된다.
 }
 
 function handleSubmit(event){
@@ -26,9 +42,13 @@ function handleSubmit(event){
 }
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos === null){
-        console.log("null")
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){ 
+            paintTodo(toDo.text);
+        })
+        
     }else{
         console.log("in something");
     }
